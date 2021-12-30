0fe384ed6e4c58cd00aae2ba4eae3f5bc9a771e2
walter <1654226880301@ingenieria.usac.edu.gt>
2021-12-30 16:44:39 -0600

    riakct :v

---
+++ src/components/checkout/Checkout.component.js
@@ -0,0 +1,33 @@
+
+import React, { useState, useRef, useEffect } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Button } from 'bootstrap';
+import './Checkout.css';
+//import $ from 'jquery';
+
+
+
+
+
+const Checkout = ({ title, show,handleShow, handleClose }) => {
+
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    )
+}
+
+export default Checkout;
--- src/components/menu/Menu.component.js
+++ src/components/menu/Menu.component.js
@@ -1,25 +1,54 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { Button } from 'bootstrap';
 import './Menu.css';
 
+import Checkout from "../checkout/Checkout.component";
+
+
+
 const Menu = () => {
-    return <header>
+    const [show, setShow] = useState(false);
 
-        <nav>
-            <ul>
-                <li><a href="#">CATALOGO</a></li>
-            </ul>
-        </nav>
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <header>
 
-        <button type="button" className="btn btn-primary" css="margin: 5px;" type="button"><i className="fas fa-shopping-cart"></i></button>
-        <div className="input-group">
+            <nav>
+                <ul>
+                    <li><a href="#">CATALOGO</a></li>
+                </ul>
+            </nav>
 
-            <input type="text" className="form-control" placeholder="Buscar..." aria-label="Recipient's username" aria-describedby="basic-addon2" />
-            <button type="button" className="btn btn-secondary" css="margin-right: 5px;" type="button"><i className="fas fa-search"></i></button>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
 
-        </div>
 
-    </header>
 
+            <button type="button" className="btn btn-primary" style={{ margin: "10px" }} type="button" onClick={handleShow}><i className="fas fa-shopping-cart"></i></button>
+            <div className="input-group">
+                <input type="text" className="form-control" placeholder="Buscar..." aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                <button type="button" className="btn btn-secondary" css="margin-right: 5px;" type="button"><i className="fas fa-search"></i></button>
+            </div>
+
+            
+
+        </header>
+
+    )
 }
 
 export default Menu;
