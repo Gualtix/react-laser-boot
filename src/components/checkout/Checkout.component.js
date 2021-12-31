f61fe051f9220352738bb96d7146da081d5421bc
walter <1654226880301@ingenieria.usac.edu.gt>
2021-12-31 12:42:43 -0600

    homs

--- src/components/checkout/Checkout.component.js
+++ src/components/checkout/Checkout.component.js
@@ -1,31 +1,132 @@
 
 import React, { useState, useRef, useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
-import { Button } from 'bootstrap';
+import { Button } from 'react-bootstrap';
 import './Checkout.css';
 
+const Checkout = ({ show, handleShow, handleClose }) => {
 
+  return (
+    <div>
+      <button type="button" className="btn btn-primary" onClick={handleShow} style={{ marginRight: "5px" }}><i className="fas fa-shopping-cart"></i></button>
 
-
-const Checkout = ({ title, show,handleShow, handleClose }) => {
-
-
-    return (
-        <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>{title}</Modal.Title>
+          <Modal.Title>Confirmar Compra</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+
+
+
+          <form>
+
+            <div className="form-group">
+              <label for="exampleInputEmail1">Nombre</label>
+              <input type="email" className="form-control" id="exampleInputEmail1"/>
+            </div>
+
+            <div className="row">
+              <div className="col">
+                <label for="exampleInputEmail1">Email</label>
+                <input type="email" className="form-control" id="exampleInputEmail1"/>
+              </div>
+              <div className="col">
+                <label for="exampleInputPassword1">Telefono</label>
+                <input type="password" className="form-control" id="exampleInputPassword1"/>
+              </div>
+            </div>
+
+
+            <div className="form-group">
+              <label for="exampleInputPassword1">Direccion</label>
+              <input type="password" className="form-control" id="exampleInputPassword1"/>
+            </div>
+          </form>
+
+          <div style="display: flex; justify-content: space-between; align-items: center; background-color: rgb(255, 255, 255); margin-left: 15px; margin-top: 10px;">
+
+            <div style="font-size: 25px; font-weight: bold; background-color: rgb(255, 255, 255);">
+              Detalle
+            </div>
+
+
+          </div>
+
+          <div>
+
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Codigo</th>
+                  <th scope="col">Descripcion</th>
+                  <th scope="col">Unidades</th>
+                  <th scope="col">Subtotal</th>
+                  <th scope="col">Eliminar</th>
+                </tr>
+              </thead>
+              <tbody>
+
+              
+              <tr>
+                <th scope="row">1</th>
+                <td>Norco MT Bike</td>
+                <td>
+
+                  <div className="input-group" style="width: 150px; margin-right: 15px; ">
+                    <button type="button" className="btn btn-secondary" type="button"><i className="fas fa-minus"></i></button>
+                    <input type="text"  className="form-control"/>
+                    <button type="button" className="btn btn-secondary" type="button"><i className="fas fa-plus"></i></button>
+                  </div>
+
+                </td>
+                <td>Q 475.25</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Dunlop 29 Tires</td>
+                <td>3</td>
+                <td>Q 218.21</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Oakley MTB Helmet</td>
+                <td>7</td>
+                <td>Q 375.26</td>
+              </tr>
+            </tbody>
+
+
+
+              <tfoot>
+                <tr>
+                  <td colspan="2">Totales:</td>
+                  <td id="total_unidades">0</td>
+                  <td id="total">0</td>
+                  <td></td>
+                </tr>
+              </tfoot>
+
+            </table>
+
+          </div>
+
+
+
+
+
+
+
+
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
+
+          <Button variant="secondary" onClick={handleClose}>Cancelar</Button>
+          <Button variant="primary" onClick={handleClose}>Realizar Compra</Button>
+
         </Modal.Footer>
       </Modal>
-    )
+    </div>
+  )
 }
 
 export default Checkout;
--- src/components/menu/Menu.component.js
+++ src/components/menu/Menu.component.js
@@ -1,9 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
-import { Button } from 'bootstrap';
+import { Button } from 'react-bootstrap';
 import './Menu.css';
 
-//import Checkout from "../checkout/Checkout.component";
+import Checkout from "../checkout/Checkout.component";
 
 
 
@@ -21,34 +21,16 @@ const Menu = () => {
                 </ul>
             </nav>
 
-            <Button variant="primary" onClick={handleShow}>Launch demo modal</Button>
+            <Checkout
+                show={show}
+                setShow={setShow}
+                handleClose={handleClose}
+                handleShow={handleShow}
+            />
 
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-
-            
-
-
-
-            
             <div className="input-group">
                 <input type="text" className="form-control" placeholder="Buscar..." aria-label="Recipient's username" aria-describedby="basic-addon2" />
-
             </div>
-
-            
 
         </header>
 
@@ -56,3 +38,10 @@ const Menu = () => {
 }
 
 export default Menu;
+
+
+/*
+  
+
+
+*/
