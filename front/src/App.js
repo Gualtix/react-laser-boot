3a0c96e1cec1f39bfc7b57a4fbf9f0ce30fd5aec
walter <1654226880301@ingenieria.usac.edu.gt>
2022-01-13 02:52:27 -0600

    fix

--- front/src/App.js
+++ front/src/App.js
@@ -7,7 +7,15 @@ import './App.css';
 
 
 const App = () => {
-  return <div className="whole">
+  return (
+    
+  <div className="whole">
+
+    <div style={{display: "flex",justifyContent:"center",backgroundColor:"#0E101C"}}>
+      <img src="https://i.pinimg.com/originals/85/c8/3d/85c83d242b73d3c52b8c4c2e7df27db7.jpg" 
+      style={{width: "305px"}}/>
+    
+    </div>
     <div className="main">
       <Menu/>
       <section className="sect">
@@ -19,7 +27,7 @@ const App = () => {
       <Footer/>
 
     </div>
-  </div>
+  </div>);
 }
 
 
